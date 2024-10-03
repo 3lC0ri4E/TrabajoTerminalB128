aa75caaa3baa9e527b713423335b981c6b8601e2
3lC0ri4E <coria1820.escom@gmail.com>
2024-10-02 18:34:34 -0600

    agrega vistas de empresa

---
+++ CyptoPredictWeb/crypto-predict/src/pages/QuienesSomos.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react';
+import Navbar from './Navbar';
+import { Box, Text } from '@chakra-ui/react';
+
+export default function QuienesSomos() {
+	return (
+		<Box>
+			<Navbar />
+
+			<Text>Quienes Somos</Text>
+		</Box>
+	);
+}
