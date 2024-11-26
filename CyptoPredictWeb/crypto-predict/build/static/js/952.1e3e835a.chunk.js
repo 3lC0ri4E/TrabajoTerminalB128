10359cc75f1bf7081c09df22702c5904f57ea3c7
3lC0ri4E <coria1820.escom@gmail.com>
2024-11-25 19:16:49 -0600

    upload build dir

---
+++ CyptoPredictWeb/crypto-predict/build/static/js/952.1e3e835a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcrypto_predict=self.webpackChunkcrypto_predict||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.1e3e835a.chunk.js.map
