00a7a442d1543dd290ea0db543743e5ae9beb07f
3lC0ri4E <coria1820.escom@gmail.com>
2024-11-27 08:25:36 -0600

    Fixed enviroment variables

--- CyptoPredictWeb/crypto-predict/src/supabase/client.js
+++ CyptoPredictWeb/crypto-predict/src/supabase/client.js
@@ -3,8 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Acceder a las variables de entorno
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://ktblaawjnhtkogqmqhmw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0YmxhYXdqbmh0a29ncW1xaG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwOTM4MTQsImV4cCI6MjAyNzY2OTgxNH0.NyBKLUCMVzJ5sP8x4GgGvccN7D6-SIJ2vgn7cnXV9xc';
 
 // Verificar si las variables están definidas
 if (!supabaseUrl) {
