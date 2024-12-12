a8c729590086d8c9270f5a1bc4549143257e57e3
Yngasarock <asaelgarcia360@outlook.com>
2024-12-12 15:43:44 -0600

    cambios js

--- CyptoPredictWeb/crypto-predict/src/supabase/supabase_functions.js
+++ CyptoPredictWeb/crypto-predict/src/supabase/supabase_functions.js
@@ -94,8 +94,41 @@ export async function signIn(email, password) {
 	return { data, error };
 }
 
-
 export async function sendResetPasswordEmail(email) {
 	const { error } = await supabase.auth.resetPasswordForEmail(email, { redirectTo: 'http://localhost:3000/ajustes', })
 	return { error };
 }
+
+// Insertar noticias en la base de datos
+export async function uploadnews(data) {
+	const { data: news, error } = await supabase.from('news').insert(data);
+	return { news, error };
+}
+
+// Obtener noticias de la base de datos 
+export async function getnews(currentDate) {
+	const { data, error } = await supabase.from('news').select('*').gte('pubDate', currentDate);
+	return { data, error };
+}
+
+// Insertar label de sentimiento y valor de probabilidad
+export async function insertSentiment(created_at, label, probability) {
+	const { data, error } = await supabase.from('analisis_fundamental').insert({ created_at, label, probability });
+	return { data, error };
+}
+/*
+// Obtener el id de analisis_fundamental
+export async function getNewsAnalysisId() {
+	const { data, error } = await supabase.from('analisis_fundamental').select('id');
+	return { data, error };
+}
+
+// Actualizar analisis_id de noticias
+export async function updateNewsAnalysisId(newsId, analisisId) {
+	const { data, error } = await supabase
+	  .from('news')
+	  .update({ analisis_id: analisisId })
+	  .eq('id', newsId);
+  
+	return { data, error };
+}*/
